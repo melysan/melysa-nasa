6b637eae98693c478b0ca545bdbad475995b1481
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-02-14 15:19:27 -0800

    initial commit - in class

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["technology.nasa.gov", "epic.gsfc.nasa.gov"]
+  }
+
+}
