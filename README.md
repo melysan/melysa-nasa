8b45da2a6d5a469744245834cc5455514ea9c189
MelysaNguyen <mnguyen181@my.bcit.ca>
2023-02-19 19:57:57 -0800

    nasa styling

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Installation
-npm i axios
+# About NASA Trek
+NASA Trek is a web app that uses NASA's open APIs: EPIC and TechTransfer endpoints.
+
+"TechTransfer provides structured, searchable developer access to NASA’s patents, software, and technology spinoff descriptions that have been curated to support technology transfer." 
+
+"EPIC provides information on the daily imagery collected by DSCOVR's Earth Polychromatic Imaging Camera (EPIC) instrument."
+
+
+
+# Techology and Tools Used
+- Next JS 13
+- Axios
+- [NASA Open API](https://api.nasa.gov/)
+
+# Installation & How to Run
+> npm i
+
+axios:
+> npm i axios
+
+to run application:
+> npm run dev
--- pages/polychromatic.js
+++ pages/polychromatic.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Image from "next/image";
+import styles from '@/styles/Home.module.css';
+import Link from "next/link";
+import Head from "next/head";
 
 
 export default function Polychromatic() {
@@ -57,46 +60,63 @@ export default function Polychromatic() {
 
     return (
         <>
-            Polychromatic
+            <Head>
+                <title>NASA Trek - Polychromatic</title>
+                <meta name="description" content="NASA API Web App" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-            <Image src={image} alt={image} width={200} height={200} />
-            <div>{time}</div>
-            <div>{coords[0]}, {coords[1]}</div>
+            <Link className={styles.polychromaticLink} href={"/"}>Warp to Home</Link>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Time</th>
-                        <th>Latitude</th>
-                        <th>Longitude</th>
-                        <th>Image</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        images.map((e, i) => {
-                            return (
-                                <tr key={i}>
-                                    <td>{e.time}</td>
-                                    <td>{e.coords.lat}</td>
-                                    <td>{e.coords.lon}</td>
-                                    <td><Image src={e.image} alt={i} width={200} height={200} /></td>
-                                    <td>
-                                        <button onClick={() => {
-                                            setImage(e.image);
-                                            setTime(e.time);
-                                            setCoords([e.coords.lat, e.coords.lon]);
-                                            console.log(images[i]);
-                                            document.body.scrollIntoView();
-                                        }}>View</button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
+            <div className={styles.mainPoly}>
+
+
+                <div className={styles.polyMainImageCont}>
+                    <h1 className={styles.polyHeader}>Polychromatic</h1>
+                    <Image className={styles.polyMainImage}
+                        src={image} alt={image} width={300} height={300} />
+                    <div>{time}</div>
+                    <div>{coords[0]}, {coords[1]}</div>
+                </div>
+
+                <table className={styles.polyTable}>
+                    <thead>
+                        <tr>
+                            <th className={styles.polyTableHead}>Time</th>
+                            <th className={styles.polyTableHead}>Latitude</th>
+                            <th className={styles.polyTableHead}>Longitude</th>
+                            <th className={styles.polyTableHead}>Image</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            images.map((e, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td className={styles.polyTableHead}>{e.time}</td>
+                                        <td className={styles.polyTableHead}>{e.coords.lat}</td>
+                                        <td className={styles.polyTableHead}>{e.coords.lon}</td>
+                                        <td><Image className={styles.polyMainImage} src={e.image} alt={i} width={200} height={200} /></td>
+                                        <td>
+                                            <button
+                                                className={styles.polyButton}
+                                                onClick={() => {
+                                                    setImage(e.image);
+                                                    setTime(e.time);
+                                                    setCoords([e.coords.lat, e.coords.lon]);
+                                                    console.log(images[i]);
+                                                    document.body.scrollIntoView();
+                                                }}>View</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
